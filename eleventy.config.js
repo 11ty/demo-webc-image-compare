22511942a58b95d51087a20d5290df29d6c3bf69
Zach Leatherman <zachleatherman@gmail.com>
2022-10-24 17:10:12 -0500

    Syntax highlighting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,8 @@
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(syntaxHighlight);
 	eleventyConfig.addPlugin(pluginWebc, {
 		components: "_includes/webc/*.webc"
 	});
