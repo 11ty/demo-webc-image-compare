d06e59803e0eefe7c04ec389a37f36f21374b8c0
Zach Leatherman <zachleatherman@gmail.com>
2022-11-18 21:32:41 -0600

    Update to the newest version

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,8 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
+	eleventyConfig.ignores.add("README.md");
+
 	eleventyConfig.addPlugin(syntaxHighlight);
 	eleventyConfig.addPlugin(pluginWebc);
 
